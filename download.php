4b54932d0e54dbaa507b622bbb6faa89abc90e16
chamarasab <chamara.sab@outlook.com>
2024-07-08 16:57:00 +0530

    first commit

---
+++ download.php
@@ -0,0 +1,55 @@
+<?php
+require_once('vendor/autoload.php');
+
+// Create new PDF document
+$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+
+// Set document information
+$pdf->SetCreator(PDF_CREATOR);
+$pdf->SetAuthor('Your Name');
+$pdf->SetTitle('Password Protected PDF');
+$pdf->SetSubject('TCPDF Tutorial');
+$pdf->SetKeywords('TCPDF, PDF, example, test, guide');
+
+// Set default header data
+$pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE.' 001', PDF_HEADER_STRING);
+
+// Set header and footer fonts
+$pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
+$pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
+
+// Set default monospaced font
+$pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+
+// Set margins
+$pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+$pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
+$pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
+
+// Set auto page breaks
+$pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
+
+// Set image scale factor
+$pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+
+// Set font
+$pdf->SetFont('helvetica', '', 12);
+
+// Add a page
+$pdf->AddPage();
+
+// Set some content to print
+$html = '<h1>Welcome to TCPDF!</h1>
+<p>This is an example of a password-protected PDF using TCPDF library in PHP.</p>';
+
+// Print text using writeHTMLCell()
+$pdf->writeHTMLCell(0, 0, '', '', $html, 0, 1, 0, true, '', true);
+
+// Set the password
+$pdf->SetProtection(array('print', 'copy'), '1234', null, 0, null);
+
+// Close and output PDF document
+$pdf->Output('protected_pdf.pdf', 'D');
+
+include_once "index2.html";
+?>
